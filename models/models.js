5b93974c0b89abee45eb5c5334c53eceae8cfa03
Vanesa Martin <vanesa.martindecima@gmail.com>
2015-07-10 00:07:05 +0200

    Añadir base de datos local

---
+++ models/models.js
@@ -0,0 +1,29 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite 
+var sequelize = new Sequelize(null, null, null, 
+  { dialect:  "sqlite",
+    storage:  "quiz.sqlite"
+  }      
+);
+
+// Importar definicion de la tabla Quiz
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+
+// exportar tablas
+exports.Quiz = Quiz; 
+
+// sequelize.sync() inicializa tabla de preguntas en DB
+sequelize.sync().success(function() {
+    Quiz.count().success(function (count){
+        if(count === 0) {   // la tabla se inicializa solo si está vacía
+            Quiz.create({
+                pregunta: 'Capital de Italia',   respuesta: 'Roma'                
+            })
+            .success(function() {console.log('Base de datos inicializada')});
+        };
+    });
+});
