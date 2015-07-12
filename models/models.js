2e710e1cba8dfce7826f8c0230d48308e49af35a
Vanesa Martin <vanesa.martindecima@gmail.com>
2015-07-13 00:24:13 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -33,13 +33,16 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; 
 
 // sequelize.sync() inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
-    Quiz.count().success(function (count){
+sequelize.sync().then(function() {
+    Quiz.count().then(function (count){
         if(count === 0) {   // la tabla se inicializa solo si está vacía
             Quiz.create({
                 pregunta: 'Capital de Italia',   respuesta: 'Roma'                
             })
-            .success(function() {console.log('Base de datos inicializada')});
+            Quiz.create({
+                pregunta: 'Capital de Portugal',   respuesta: 'Lisboa'                
+            })
+            .then(function() {console.log('Base de datos inicializada')});
         };
     });
 });
