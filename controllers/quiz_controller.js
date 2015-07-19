cc1ed9d4c8ec3a08076d0fe577cae7e6d5d17e2d
Vanesa Martin <vanesa.martindecima@gmail.com>
2015-07-19 22:44:39 +0200

    Editar y borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -56,25 +56,6 @@ exports.new = function(req, res)
     res.render('quizes/new', { quiz: quiz, errors: []}); 
 };
 
-//POST /quizes/create
-//exports.create = function(req, res) { 
-//  var quiz = models.Quiz.build( req.body.quiz );
-// console.log("vanesa " + quiz.validate());
- // quiz
- // .validate()
- // .then(
- //   function(err){
- //     if (err) {
- //       res.render('quizes/new', {quiz: quiz, errors: err.errors});
- //     } else {
- //       quiz // save: guarda en DB campos pregunta y respuesta de quiz
- //       .save({fields: ["pregunta", "respuesta", "UserId", "image"]})
- //       .then( function(){ res.redirect('/quizes')}) 
- //     }      // res.redirect: Redirección HTTP a lista de preguntas
- //   }
-//).catch(function(error){next(error)});
-//};
-
 exports.create = function(req, res){
     var quiz = models.Quiz.build( req.body.quiz );
 
@@ -89,4 +70,37 @@ exports.create = function(req, res){
         .save({fields: ["pregunta", "respuesta"]})
         .then( function(){ res.redirect('/quizes')}) ;
     }
+};
+
+//GET /quizes/:id/edit
+exports.edit = function(req, res)
+{
+    var quiz = req.quiz; //autoload de instancia de quiz
+
+    res.render('quizes/edit', { quiz: quiz, errors: []}); 
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res){
+    req.quiz.pregunta = req.body.quiz.pregunta;
+    req.quiz.respuesta = req.body.quiz.respuesta;
+
+    var errors = req.quiz.validate();//ya qe el objeto errors no tiene then(
+    if (errors)
+    {
+        var i=0; var errores=new Array();//se convierte en [] con la propiedad message por compatibilida con layout
+        for (var prop in errors) errores[i++]={message: errors[prop]};  
+            res.render('quizes/edit', {quiz: req.quiz, errors: errores});
+    } else {
+        req.quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) ;
+    }
+};
+
+//DELETE /quizes/:id
+exports.destroy = function(req, res) {
+    req.quiz.destroy().then( function() {
+        res.redirect('/quizes');
+    }).catch(function (error) { next(error)});
 };
